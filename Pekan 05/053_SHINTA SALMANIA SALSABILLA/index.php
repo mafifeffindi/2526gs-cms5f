0396f791fa94ee0092deb4367e3ca3d1a64044e8
salsaa131204 <shintasalmanias@gmail.com>
2025-09-17 11:18:38 +0700

    Update index.php

--- Pekan 05/053_SHINTA SALMANIA SALSABILLA/index.php
+++ Pekan 05/053_SHINTA SALMANIA SALSABILLA/index.php
@@ -1,1 +1,23 @@
+<?php get_header(); ?>
+
+<div class="container">
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            
+            <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <p><small>Diposting pada <?php echo get_the_date(); ?> oleh <?php the_author(); ?></small></p>
+            <div><?php the_excerpt(); ?></div>
+            <hr>
+
+        <?php endwhile; ?>
+
+        <div class="pagination">
+            <?php posts_nav_link(' | ', '« Post Sebelumnya', 'Post Selanjutnya »'); ?>
+        </div>
+    <?php else : ?>
+        <h2>Tidak ada postingan ditemukan.</h2>
+    <?php endif; ?>
+</div>
+
+<?php get_footer(); ?>
 
