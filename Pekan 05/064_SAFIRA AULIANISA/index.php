ed17130dc312fe7f113011791da8b809f4e7a94e
SafiraAulianisa <safiraakhmad42@gmail.com>
2025-09-19 06:40:03 +0700

    Update index.php

--- Pekan 05/064_SAFIRA AULIANISA/index.php
+++ Pekan 05/064_SAFIRA AULIANISA/index.php
@@ -1,1 +1,31 @@
+<?php get_header(); ?> 
 
+<main id="main-content">
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h2 class="post-title">
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+                <div class="post-meta">
+                    Diposting pada <?php echo get_the_date(); ?> oleh <?php the_author(); ?>
+                </div>
+                <div class="post-content">
+                    <?php the_excerpt(); ?>
+                </div>
+            </article>
+
+        <?php endwhile; ?>
+
+        <div class="pagination">
+            <?php the_posts_pagination(); ?>
+        </div>
+
+    <?php else : ?>
+        <p>Tidak ada postingan ditemukan.</p>
+    <?php endif; ?>
+</main>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
