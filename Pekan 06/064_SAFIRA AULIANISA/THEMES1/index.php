4ffe00df6c6700644287bc03159f7e0d1ac96eb7
SafiraAulianisa <safiraakhmad42@gmail.com>
2025-09-24 11:53:33 +0700

    Update index.php

--- Pekan 06/064_SAFIRA AULIANISA/THEMES1/index.php
+++ Pekan 06/064_SAFIRA AULIANISA/THEMES1/index.php
@@ -1,1 +1,21 @@
+<?php get_header(); ?>
 
+<main>
+    <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : the_post(); ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                <div class="entry-meta">
+                    <span>Ditulis oleh <?php the_author(); ?> pada <?php the_time('d M Y'); ?></span>
+                </div>
+                <div class="entry-content">
+                    <?php the_excerpt(); ?>
+                </div>
+            </article>
+        <?php endwhile; ?>
+    <?php else : ?>
+        <p>Belum ada postingan ditemukan.</p>
+    <?php endif; ?>
+</main>
+
+<?php get_footer(); ?>
