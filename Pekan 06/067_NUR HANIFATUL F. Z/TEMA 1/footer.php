8e7247c9b6bb1b5a1882eb6c59350ed3bc28a133
NurHanifatulFitriyahZamrudah <fitriyahnur928@gmail.com>
2025-10-01 11:51:12 +0700

    Create footer.php

---
+++ Pekan 06/067_NUR HANIFATUL F. Z/TEMA 1/footer.php
@@ -0,0 +1,35 @@
+<footer class="site-footer">
+  <div class="footer-top">
+    <div class="footer-logo">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo.png" alt="Logo UTM">
+    </div>
+    <ul class="footer-menu">
+      <li><a href="#">Cara Melamar</a></li>
+      <li><a href="#">Penerimaan</a></li>
+      <li><a href="#">Beasiswa</a></li>
+      <li><a href="#">Tentang Kami</a></li>
+      <li><a href="#">Program</a></li>
+      <li><a href="#">Berita & Acara</a></li>
+      <li><a href="#">Kontak</a></li>
+    </ul>
+  </div>
+
+  <div class="footer-content">
+    <div class="footer-info">
+      <p>📍 Jalan Raya Telang, Kamal, Bangkalan, Madura, Jawa Timur</p>
+      <p>📧 info@trunojoyo.ac.id</p>
+      <p>☎ (031) 3011146</p>
+    </div>
+    <div class="footer-copy">
+      <p>© 2025 Universitas Trunojoyo Madura. All rights reserved.</p>
+    </div>
+  </div>
+  </div>
+
+  <div class="footer-bottom">
+    <p>Hak Cipta © <?php echo date("Y"); ?> Universitas Trunojoyo Madura. Seluruh hak cipta dilindungi undang-undang.</p>
+  </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
